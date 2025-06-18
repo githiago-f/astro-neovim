ae8998706be0e0f8b6af20f60bde6f4af6d59aba
githiago-f <thiagodutra@cartaodetodos.com>
2025-06-18 08:50:48 -0300

    fix: project dir

--- scripts/project-dir.sh
+++ scripts/project-dir.sh
@@ -1,3 +1,11 @@
-project=`fd -d 1 . ~/projects | grep -v 'key-secret' | fzf`
+if command -v fd &> /dev/null; then
+  find_p="fd"
+elif command -v fdfind &> /dev/null; then
+  find_p="fdfind"
+else
+  find_p="find"
+fi
+
+project=`$find_p -d 1 . ~/projects | grep -v 'key-secret' | fzf`
 echo $project
 
