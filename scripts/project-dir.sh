d33dbc0c827906c2ccf9bb7a80f989801edb20c7
githiago-f <tfarias@protonmail.com>
2025-06-26 20:32:08 -0300

    fix: open project and config

--- scripts/project-dir.sh
+++ scripts/project-dir.sh
@@ -1,3 +1,4 @@
-project=`fd -d 1 . ~/projects | grep -v 'key-secret' | fzf`
-echo $project
+projects=`fd -d 1 . ~/projects | grep -v 'key-secret'`
+configs=`echo "hypr kitty nvim waybar wlogout ml4w ml4w-hyprland-settings" | tr ' ' '\n' | awk '{print ENVIRON["HOME"] "/.config/" $1}'`
+echo `printf "$projects\n$configs" | fzf`
 
